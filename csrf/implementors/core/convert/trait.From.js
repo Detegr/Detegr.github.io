76d336d2d8aac26597cf2d7603a0d385a4999c12
Antti Keränen <detegr@gmail.com>
2016-08-19 23:10:26 +0300

    Add csrf documentation

---
+++ csrf/implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["base64"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='https://doc.rust-lang.org/nightly/collections/string/struct.FromUtf8Error.html' title='collections::string::FromUtf8Error'>FromUtf8Error</a>&gt; for <a class='enum' href='base64/enum.Base64Error.html' title='base64::Base64Error'>Base64Error</a>",];implementors["libc"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
