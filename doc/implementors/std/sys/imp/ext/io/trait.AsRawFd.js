12941702cac0217f7f3cd9e2a1e2605cc51e1a39
Antti Keränen <detegr@gmail.com>
2018-01-14 22:09:50 +0200

    Update CtrlC documentation

--- doc/implementors/std/sys/imp/ext/io/trait.AsRawFd.js
+++ doc/implementors/std/sys/imp/ext/io/trait.AsRawFd.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["libc"] = [];
-implementors["nix"] = [];
+implementors["nix"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/imp/ext/io/trait.AsRawFd.html\" title=\"trait std::sys::imp::ext::io::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"nix/pty/struct.PtyMaster.html\" title=\"struct nix::pty::PtyMaster\">PtyMaster</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/imp/ext/io/trait.AsRawFd.html\" title=\"trait std::sys::imp::ext::io::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"nix/sys/signalfd/struct.SignalFd.html\" title=\"struct nix::sys::signalfd::SignalFd\">SignalFd</a>",];
 implementors["void"] = [];
 
             if (window.register_implementors) {
--- doc/implementors/std/sys/imp/ext/io/trait.IntoRawFd.js
+++ doc/implementors/std/sys/imp/ext/io/trait.IntoRawFd.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["libc"] = [];
-implementors["nix"] = [];
+implementors["nix"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/imp/ext/io/trait.IntoRawFd.html\" title=\"trait std::sys::imp::ext::io::IntoRawFd\">IntoRawFd</a> for <a class=\"struct\" href=\"nix/pty/struct.PtyMaster.html\" title=\"struct nix::pty::PtyMaster\">PtyMaster</a>",];
 implementors["void"] = [];
 
             if (window.register_implementors) {
